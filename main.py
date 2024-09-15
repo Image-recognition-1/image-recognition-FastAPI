c172c26ba909583588f8d5e31a06e440889cd403
Mazulj11 <mateo.zulj11@gmail.com>
2024-09-15 15:40:39 +0200

    implement firebase login and register

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import json
-from typing import List, Literal, Optional
 from fastapi import FastAPI, Query, Request, HTTPException, Depends, Security
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
@@ -7,12 +5,7 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer, OAuth2Pas
 from pydantic import BaseModel
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
-import ssl
 import requests
-from datetime import datetime
-
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain('./cert.pem', keyfile='./key.pem')
 
 cred = credentials.Certificate('./firebase-admin-sdk.json')
 firebase_admin.initialize_app(cred)
@@ -21,9 +14,7 @@ db = firestore.client()
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:8081",
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -40,17 +31,14 @@ class LoginRequest(BaseModel):
     email: str
     password: str
 
-class Purchase(BaseModel):
-    payment_method: str
-    purchase_date: datetime
-    token_amount: int
-    user_id: str
+class ResponseUser(BaseModel):
+    uid: str
+    email: str
+    fullName: str
+    username: str
+    role: str
+    disabled: bool
 
-class Expense(BaseModel):
-    expense_date: datetime
-    image_id: str
-    token_amount: int
-    user_id: str
 
 async def verify_token(credentials: HTTPAuthorizationCredentials = Security(HTTPBearer())):
     token = credentials.credentials
@@ -63,7 +51,7 @@ async def verify_token(credentials: HTTPAuthorizationCredentials = Security(HTTP
 @app.post("/login")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     try:
-        api_key = "AIzaSyAXR9sM7XtgKdCeP zfL76Siifoa_UPCstE"
+        api_key = "AIzaSyDv2mI6PhvFB4yCIxFsq2JmEt8Gn0aKpAQ"
         url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={api_key}"
         
         payload = {
@@ -82,6 +70,65 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Login failed: {e}")
+    
+
+class RegisterRequest(BaseModel):
+    email: str
+    password: str
+    fullName: str
+    username: str
+    role: str = 'USER'
+    disabled: bool = False
+
+@app.post("/register")
+async def register_user(register_request: RegisterRequest):
+    try:
+        user = auth.create_user(
+            email=register_request.email,
+            password=register_request.password,
+            display_name=register_request.fullName,
+            disabled=register_request.disabled
+        )
+        
+        user_data = {
+            'uid': user.uid,
+            'email': register_request.email,
+            'fullName': register_request.fullName,
+            'username': register_request.username,
+            'role': register_request.role,
+            'disabled': register_request.disabled
+        }
+        
+        db.collection('users').document(user.uid).set(user_data)
+        
+        return {"message": f"User {register_request.email} registered successfully"}
+    
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error registering user: {str(e)}")
+
+@app.get("/getme", response_model=ResponseUser)
+async def get_me(request: Request):
+    try:
+        auth_header = request.headers.get('Authorization')
+        if not auth_header or not auth_header.startswith("Bearer "):
+            raise HTTPException(status_code=401, detail="Missing or invalid Authorization header")
+        
+        token = auth_header.split(" ")[1]
+
+        decoded_token = auth.verify_id_token(token)
+        user_uid = decoded_token.get('uid')
+
+        user_ref = db.collection('users').document(user_uid)
+        user_doc = user_ref.get()
+
+        if not user_doc.exists:
+            raise HTTPException(status_code=404, detail="User not found in the database")
+
+        user_data = user_doc.to_dict()
+        return {"user": user_data}
+
+    except Exception as e:
+        raise HTTPException(status_code=401, detail=f"Invalid token or error retrieving user: {str(e)}")
 
 
 @app.get("/verify-token")
@@ -101,134 +148,8 @@ async def verify_token(request: Request):
         raise HTTPException(status_code=401, detail="Invalid token")
     
 
-@app.post("/add-purchase", dependencies=[Depends(verify_token)])
-async def add_purchase(purchase: Purchase):
-    user_ref = db.collection('users').document(purchase.user_id)
-    try:
-        transaction = db.transaction()
-        @firestore.transactional
-        def update_tokens(transaction, user_ref):
-            user_doc = user_ref.get(transaction=transaction)
-            if not user_doc.exists:
-                raise HTTPException(status_code=404, detail="User does not exist")
-            new_tokens = user_doc.get('availableTokens') + purchase.token_amount
-            transaction.update(user_ref, {'availableTokens': new_tokens})
-            transaction.set(db.collection('purchases').document(), purchase.dict())
-
-        update_tokens(transaction, user_ref)
-        return {"message": f"Added {purchase.token_amount} tokens to user {purchase.user_id}"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Transaction failed: {e}")
-
-@app.post("/add-expense", dependencies=[Depends(verify_token)])
-async def add_expense(expense: Expense):
-    user_ref = db.collection('users').document(expense.user_id)
-    try:
-        transaction = db.transaction()
-        @firestore.transactional
-        def update_tokens(transaction, user_ref):
-            user_doc = user_ref.get(transaction=transaction)
-            if not user_doc.exists:
-                raise HTTPException(status_code=404, detail="User does not exist")
-            new_tokens = user_doc.get('availableTokens') - expense.token_amount
-            transaction.update(user_ref, {'availableTokens': new_tokens})
-            transaction.set(db.collection('expenses').document(), expense.dict())
-
-        update_tokens(transaction, user_ref)
-        return {"message": f"Subtracted {expense.token_amount} tokens from user {expense.user_id}"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Transaction failed: {e}")
-    
-@app.get("/get-expenses")
-async def get_expenses(
-    user_id: Optional[str] = Query(None, description="The ID of the user"),
-    expense_date: Optional[datetime] = Query(None, description="Expenses after this date (ISO 8601 format)")):
-    try:
-        expenses_ref = db.collection('expenses')
-
-        if user_id and expense_date:
-            expenses_query = expenses_ref.where('user_id', '==', user_id).where('expense_date', '>', expense_date).order_by('expense_date', direction=firestore.Query.DESCENDING)
-        elif user_id:
-            expenses_query = expenses_ref.where('user_id', '==', user_id).order_by('expense_date', direction=firestore.Query.DESCENDING)
-        elif expense_date:
-            expenses_query = expenses_ref.where('expense_date', '>', expense_date).order_by('expense_date', direction=firestore.Query.DESCENDING)
-        else:
-            expenses_query = expenses_ref.order_by('expense_date', direction=firestore.Query.DESCENDING)
-        
-        expenses = [expense.to_dict() for expense in expenses_query.stream()]
-        
-        return expenses
-            
-    except Exception as e:
-        return JSONResponse(status_code=500, content={"message": str(e)})
-
-
-class Location(BaseModel):
-    latitude: float
-    longitude: float
-
-class Place(BaseModel):
-    location: Location
-    formattedAddress: str
-    googleMapsUri: str
-    displayName: str
-
-@app.post("/search-nearby", response_model=List[Place])
-def search_nearby(
-    included_type: Literal['parking', 'electric_vehicle_charging_station'],
-    maxResultCount: int,
-    latitude: float,
-    longitude: float,
-    radius: float,
-):
-    url = 'https://places.googleapis.com/v1/places:searchNearby'
-    headers = {
-        'Content-Type': 'application/json',
-        'X-Goog-Api-Key': 'AIzaSyBo7u5Z2yJOZJSoP2ZwDEFYUSJ4hvNybmY',
-        'X-Goog-FieldMask': 'places.displayName,places.formattedAddress,places.location,places.googleMapsUri'
-    }
-    payload = {
-        "includedTypes": [included_type],
-        "maxResultCount": maxResultCount,
-        "rankPreference": "DISTANCE",
-        "locationRestriction": {
-            "circle": {
-                "center": {
-                    "latitude": latitude,
-                    "longitude": longitude
-                },
-                "radius": radius
-            }
-        },
-    }
-
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
-    
-    if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail=response.text)
-    
-    data = response.json()
-    results = []
-    
-    for place in data.get('places', []):
-        location = place.get('location', {})
-        latitude = location.get('latitude', 0.0)
-        longitude = location.get('longitude', 0.0)
-        formatted_address = place.get('formattedAddress', '')
-        google_maps_uri = place.get('googleMapsUri', '')
-        display_name = place.get('displayName', {}).get('text', '')
-
-        results.append(Place(
-            location=Location(latitude=latitude, longitude=longitude),
-            formattedAddress=formatted_address,
-            googleMapsUri=google_maps_uri,
-            displayName=display_name
-        ))
-    
-    return results
-
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8000, ssl_keyfile='./key.pem', ssl_certfile='./cert.pem', ssl_version=ssl.PROTOCOL_TLS_SERVER)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
 
 # uvicorn main:app --reload --host 127.0.0.1 --port 8000 --ssl-keyfile ./key.pem --ssl-certfile ./cert.pem
