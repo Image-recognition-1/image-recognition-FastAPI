1427303a4a2d89411cdb7a8ff6449a5b292fd521
absak <ante.bruno.sakic@fpmoz.sum.ba>
2024-09-15 21:21:56 +0200

    add-image-recognition-model

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from fastapi import FastAPI, Query, Request, HTTPException, Depends, Security, Response
+from fastapi import FastAPI, HTTPException, Depends, Response, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer, OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from keras.applications import ResNet50
 from pydantic import BaseModel
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 import requests
-from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet import preprocess_input, decode_predictions
+import numpy as np
+
 
 cred = credentials.Certificate('./firebase-admin-sdk.json')
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
-
 app = FastAPI()
 
+# CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:9000"],
@@ -22,12 +27,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# OAuth2 scheme
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+# Defining  fakin model
+
+templates = Jinja2Templates(directory="templates")
+model = ResNet50(weights='imagenet')
 
 
 class LoginRequest(BaseModel):
     email: str
     password: str
+
 
 class RegisterRequest(BaseModel):
     email: str
@@ -37,6 +49,7 @@ class RegisterRequest(BaseModel):
     role: str = 'USER'
     disabled: bool = False
 
+
 class ResponseUser(BaseModel):
     uid: str
     email: str
@@ -44,6 +57,24 @@ class ResponseUser(BaseModel):
     username: str
     role: str
     disabled: bool
+
+
+# Famozna ruta za upload slike
+@app.post("/upload")
+async def upload(request: Request, file: UploadFile = File(...)):
+    try:
+        contents = await file.read()
+        with open(file.filename, "wb") as f:
+            f.write(contents)
+        img = image.load_img(file.filename, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        preds = model.predict(x)
+        results = decode_predictions(preds, top=3)[0]
+        return templates.TemplateResponse("results.html", {"request": request, "results": results})
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Error processing file: {str(e)}")
 
 
 @app.post("/login")
@@ -62,9 +93,9 @@ async def login(response: Response, form_data: OAuth2PasswordRequestForm = Depen
         response_data = api_response.json()
         
         if api_response.status_code != 200:
-            raise HTTPException(status_code=api_response.status_code, detail=response_data.get("error", {}).get("message", "Login failed"))
-        
-        
+            raise HTTPException(status_code=api_response.status_code, detail=response_data
+                                .get("error", {}).get("message", "Login failed"))
+
         response.set_cookie(key="Token", value=response_data["idToken"], httponly=True, secure=True)
         
         return {"message": "Login successful"}
@@ -106,7 +137,8 @@ async def register_user(response: Response, register_request: RegisterRequest):
         response_data = api_response.json()
 
         if api_response.status_code != 200:
-            raise HTTPException(status_code=api_response.status_code, detail=response_data.get("error", {}).get("message", "Login failed after registration"))
+            raise HTTPException(status_code=api_response.status_code, detail=response_data.get("error", {})
+                                .get("message", "Login failed after registration"))
 
         response.set_cookie(key="Token", value=response_data["idToken"], httponly=True, secure=True)
         
@@ -114,6 +146,7 @@ async def register_user(response: Response, register_request: RegisterRequest):
     
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error registering user: {str(e)}")
+
 
 @app.get("/getme", response_model=ResponseUser)
 async def get_me(request: Request):
@@ -149,11 +182,6 @@ async def get_me(request: Request):
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error retrieving user: {str(e)}")
-
-
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Error retrieving user: {str(e)}")
-
 
 
 if __name__ == '__main__':
